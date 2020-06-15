74d8b91f6f507b5633326680ef1de243cb9b01c1
pyBae-py <sajeel.ahmed@protonmail.com>
2020-06-15 14:41:47 +0500

    :new: Fix Admin

--- src/components/admin/AdminSignin.jsx
+++ src/components/admin/AdminSignin.jsx
@@ -1,32 +1,29 @@
-import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
+import React, {Fragment, useState} from 'react';
+import {toast} from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 toast.configure();
-const AdminSignin = ({ setAdminAuth }) => {
+const AdminSignin = ({setAdminAuth}) => {
   const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
-  const { email, password } = inputs;
+  const {email, password} = inputs;
 
-  const onChange = (e) =>
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  const onChange = e => setInputs({...inputs, [e.target.name]: e.target.value});
 
-  const onSubmitForm = async (e) => {
+  const onSubmitForm = async e => {
     e.preventDefault();
     try {
-
-      const body = { email, password };
+      const body = {email, password};
       const response = await fetch(
-        "http://localhost:5000/admin/authentication/login",
+        'http://localhost:5000/admin/authentication/login',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-type": "application/json",
+            'Content-type': 'application/json',
           },
           body: JSON.stringify(body),
         }
@@ -35,9 +32,9 @@ const AdminSignin = ({ setAdminAuth }) => {
       const parseRes = await response.json();
 
       if (parseRes.adminjwtToken) {
-        localStorage.setItem("admin_token", parseRes.adminjwtToken);
+        localStorage.setItem('admin_token', parseRes.adminjwtToken);
         setAdminAuth(true);
-        toast.success("Admin Logged in Successfully");
+        toast.success('Admin Logged in Successfully');
       } else {
         setAdminAuth(false);
         toast.error(parseRes);
@@ -50,34 +47,31 @@ const AdminSignin = ({ setAdminAuth }) => {
   return (
     <Fragment>
       <div
-        className='AdminSignin-wrapper'
-        style={{ width: "30%", marginLeft: "35%", marginBottom: "10.5%" }}
-      >
-        <h1 className='m-5 text-center'>Admin Login</h1>
+        className="AdminSignin-wrapper"
+        style={{width: '30%', marginLeft: '35%', marginBottom: '10.5%'}}>
+        <h1 className="m-5 text-center">Admin Login</h1>
         <form onSubmit={onSubmitForm}>
           <input
-            type='text'
-            name='email'
+            type="text"
+            name="email"
             value={email}
-            placeholder='Email Address'
-            onChange={(e) => onChange(e)}
-            className='form-control my-3'
+            placeholder="Email Address"
+            onChange={e => onChange(e)}
+            className="form-control my-3"
           />
           <input
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={password}
-            placeholder='Password'
-            onChange={(e) => onChange(e)}
-            className='form-control my-3'
+            placeholder="Password"
+            onChange={e => onChange(e)}
+            className="form-control my-3"
           />
-          <button className='btn btn-success btn-block'>Submit</button>
+          <button className="btn btn-success btn-block">Submit</button>
         </form>
         <br />
         <br />
-        <p>
-          Don't have an Account? <Link to='/admin/register'>Register</Link> here
-        </p>
+
         <br />
       </div>
     </Fragment>
